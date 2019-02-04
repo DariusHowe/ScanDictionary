07f01db867394e86ff0fa0294b4450f52c10af96
mshober <s1047084@monmouth.edu>
2019-02-04 10:15:54 -0500

    First Commit

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+
+use_frameworks!
+platform :ios, '11.0'
+
+
+target 'ScanDictionary' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ScanDictionary
+    pod 'TesseractOCRiOS'
+
+end
--- ScanDictionary/ViewController.swift
+++ ScanDictionary/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        guard let tesseract: G8Tesseract = G8Tesseract(language:"eng+ita") else { return }
+        tesseract.delegate = self
+        tesseract.charWhitelist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890"
+        tesseract.image = UIImage(named: "vrkIj.png")!
+        tesseract.recognize()
+        
+        print(tesseract.recognizedText ?? "")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func progressImageRecognition(for tesseract: G8Tesseract) {
+        print("Recognition Process \(tesseract.progress) %")
+    }
+    
+    func shouldCancelImageRecognitionForTesseract(tesseract: G8Tesseract!) -> Bool {
+        return false // return true if you need to interrupt tesseract before it finishes
+    }
 
 }
 
