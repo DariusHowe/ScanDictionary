6be769586024d3940aa6dcade9b49235daac30fd
mshober <s1047084@monmouth.edu>
2019-03-05 08:41:42 -0500

    Set camera permissions, working on image recognition.

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
     * https://github.com/gali8
 2. Web Scraping
     * https://cheerio.js.org
+3. Camera
+	* https://developer.apple.com/documentation/avfoundation/cameras_and_media_capture
+	* https://gist.github.com/tad-iizuka/fc35bc7835920c0b8b84e316f83e3a40
+	* https://medium.com/@rizwanm/https-medium-com-rizwanm-swift-camera-part-1-c38b8b773b2
+
--- ScanDictionary/ViewController.swift
+++ ScanDictionary/ViewController.swift
@@ -8,30 +8,54 @@
 
 import UIKit
 import TesseractOCR
+import AVFoundation
+import Photos
 
-class ViewController: UIViewController, G8TesseractDelegate {
+class ViewController: UIViewController, G8TesseractDelegate, AVCapturePhotoCaptureDelegate {
+
+    private let captureSession = AVCaptureSession()
+    var capturePhotoOutput: AVCapturePhotoOutput?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    @objc var captureDevice: AVCaptureDevice?
+
+    @IBOutlet var preView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let ws = WebScrapper.shared
-//        ws.analyze("testPhrase") { (score) in
-//            print(score)
-//        }
-//        
         
-//        let url = URL(string: "http://www.dictionary.com/browse/gender")!
-//
-//        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-//            guard let data = data else { return }
-//            let finalData = String(data: data, encoding: .utf8)!
-//            ws.analyze(finalData) { (score) in
-//                print(score)
-//            }
-//        }
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .authorized: // The user has previously granted access to the camera.
+            print("Authorized")
+            
+        case .notDetermined: // The user has not yet been asked for camera access.
+            AVCaptureDevice.requestAccess(for: .video) { granted in
+                if granted {
+                    print("Print Access Given")
+                }
+            }
+        case .denied: // The user has previously denied access.
+            return
+        case .restricted: // The user can't grant access due to restrictions.
+            return
+        }
 
-//        task.resume()  
+        self.captureSession.sessionPreset = .photo
+        self.capturePhotoOutput = AVCapturePhotoOutput()
         
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+        self.captureDevice = device
+        let input = try! AVCaptureDeviceInput(device: self.captureDevice!)
+        self.captureSession.addInput(input)
+        self.captureSession.addOutput(self.capturePhotoOutput!)
+        
+        self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+        self.previewLayer?.frame = self.preView.bounds
+        self.previewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        self.previewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        self.preView.layer.addSublayer(self.previewLayer!)
+        self.captureSession.startRunning()
+        
+
         
 //        guard let tesseract: G8Tesseract = G8Tesseract(language:"eng+ita") else { return }
 //        tesseract.delegate = self
@@ -45,6 +69,43 @@ class ViewController: UIViewController, G8TesseractDelegate {
 
     }
 
+    @IBAction func capture(_ sender: Any) {
+        print(#function)
+
+        let photoSettings = AVCapturePhotoSettings.init(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.flashMode = .off
+        photoSettings.isHighResolutionPhotoEnabled = false
+        self.capturePhotoOutput?.capturePhoto(with: photoSettings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        print(#function)
+     
+        guard let tesseract: G8Tesseract = G8Tesseract(language:"eng+ita") else { return }
+        guard let data = photo.fileDataRepresentation() else { return }
+        guard let image = UIImage(data: data) else { return }
+        
+        tesseract.delegate = self
+        tesseract.charWhitelist = "-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890"
+        tesseract.image = image
+        tesseract.recognize()
+
+        print(tesseract.rect)
+        print(tesseract.recognizedText ?? "")
+
+    }
+    
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput,
+                     didFinishCaptureFor resolvedSettings: AVCaptureResolvedPhotoSettings,
+                     error: Error?) {
+        print(#function)
+        guard error == nil else {
+            print("Error in capture process: \(String(describing: error))")
+            return
+        }
+    }
+    
     func progressImageRecognition(for tesseract: G8Tesseract) {
         print("Recognition Process \(tesseract.progress) %")
     }
